412f324c0d8f2de84ae35e5c23dfdbaa1fcdc103
Oleg Arbuzov <oa@kodix.ru>
2023-02-23 02:11:24 +0300

    удаляет Theme из Themeswitcher

--- src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme';
 import ThemeIcon from 'shared/assets/icons/theme-switcher.svg';
-import { Theme } from 'app/providers/ThemeProvider';
 import { Button } from 'shared/ui/Button/Button';
 
 interface ThemeSwitcherProps {
