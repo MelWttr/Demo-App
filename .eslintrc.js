f0e152eddd7956c31316c9e7325c0ff16724a0e3
Oleg Arbuzov <oa@kodix.ru>
2023-02-23 20:13:30 +0300

    правки eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,7 +38,7 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'linebreak-style': ['error', 'windows'],
+        'linebreak-style': ['warn', 'windows'],
         'i18next/no-literal-string': ['error',
             {
                 markupOnly: true,
