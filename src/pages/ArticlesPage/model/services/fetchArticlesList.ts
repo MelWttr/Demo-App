a6fe555b1a8cb66e779518346451542188094a4f
Oleg Arbuzov <oleg.arbuzov@rt.ru>
2023-06-21 16:40:20 +0300

    fix conflicts

---
+++ src/pages/ArticlesPage/model/services/fetchArticlesList.ts
@@ -0,0 +1,40 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { ThunkConfig } from 'app/providers/StoreProvider';
+import { Article } from 'entities/Article';
+import { getArticlesPageLimit } from '../selectors/articlesPageSelectors';
+
+interface fetchArticleListProps {
+    page?: number;
+}
+
+export const fetchArticlesList = createAsyncThunk<
+    Article[],
+    fetchArticleListProps,
+    ThunkConfig<string>
+    >(
+        'articlesPage/fetchArticlesList',
+        async (args, thunkApi) => {
+            const { extra, rejectWithValue, getState } = thunkApi;
+            const { page = 1 } = args;
+            const limit = getArticlesPageLimit(getState());
+
+            try {
+                const response = await extra.api.get<Article[]>('/articles', {
+                    params: {
+                        _expand: 'user',
+                        _page: page,
+                        _limit: limit,
+
+                    },
+                });
+
+                if (!response.data) {
+                    throw new Error();
+                }
+
+                return response.data;
+            } catch (e) {
+                return rejectWithValue('error');
+            }
+        },
+    );
